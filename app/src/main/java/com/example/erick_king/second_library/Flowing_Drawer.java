2603fe14f857ab67277d1448dc43dfc401d6560f
Erick-Munene <munenewahome@gmail.com>
2016-05-31 19:16:33 +0300

    added new effect

---
+++ app/src/main/java/com/example/erick_king/second_library/Flowing_Drawer.java
@@ -0,0 +1,7 @@
+package com.example.erick_king.second_library;
+
+/**
+ * Created by erick-king on 31/05/16.
+ */
+public class Flowing_Drawer {
+}
--- app/src/main/java/com/example/erick_king/second_library/MainActivity.java
+++ app/src/main/java/com/example/erick_king/second_library/MainActivity.java
@@ -1,7 +1,14 @@
 package com.example.erick_king.second_library;
 
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.mxn.soul.flowingdrawer_core.FlowingView;
+import com.mxn.soul.flowingdrawer_core.LeftDrawerLayout;
+import com.mxn.soul.flowingdrawer_core.MenuFragment;
+
+import layout.MyMenuFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        LeftDrawerLayout mLeftDrawerLayout = (LeftDrawerLayout) findViewById(R.id.id_drawerlayout);
+            FragmentManager fm = getSupportFragmentManager();
+            MyMenuFragment mMenuFragment = (MyMenuFragment) fm.findFragmentById(R.id.id_container_menu);
+            FlowingView mFlowingView = (FlowingView) findViewById(R.id.sv);
+            if (mMenuFragment == null) {
+                fm.beginTransaction().add(R.id.id_container_menu, mMenuFragment = new MyMenuFragment()).commit();
+            }
+            mLeftDrawerLayout.setFluidView(mFlowingView);
+            mLeftDrawerLayout.setMenuFragment(mMenuFragment);
+
+        }
     }
-}
+
